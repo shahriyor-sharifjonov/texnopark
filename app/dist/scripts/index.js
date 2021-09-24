c1f0d4fa30bc3ee5cec0e38ebbe01111476ab684
Shahriyor <shahriyordeveloping@gmail.com>
2021-09-24 10:13:50 +0300

    initial

--- app/dist/scripts/index.js
+++ app/dist/scripts/index.js
@@ -1,2 +1,22 @@
 "use strict";
+const headerButton =
+  document.querySelector(".intro__button");
+const headerMenu = document.querySelector(".sidebar");
+let menuOpened = false;
+const menuToggle = () => {
+  menuOpened = !menuOpened;
+  headerButton.classList.toggle("open");
+  headerMenu.classList.toggle("open");
+};
+
+headerButton.onclick = menuToggle;
+
+window.onclick = (e) => {
+  if (
+    menuOpened &&
+    !e.composedPath().includes(headerButton) &&
+    !e.composedPath().includes(headerMenu)
+  )
+    menuToggle();
+};
 //# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZXNDb250ZW50IjpbXSwiZmlsZSI6ImluZGV4LmpzIn0=
---
+++ app/src/scripts/index.ts
@@ -0,0 +1,20 @@
+const headerButton: HTMLButtonElement =
+  document.querySelector(".intro__button");
+const headerMenu: HTMLUListElement = document.querySelector(".sidebar");
+let menuOpened = false;
+const menuToggle = () => {
+  menuOpened = !menuOpened;
+  headerButton.classList.toggle("open");
+  headerMenu.classList.toggle("open");
+};
+
+headerButton.onclick = menuToggle;
+
+window.onclick = (e: MouseEvent) => {
+  if (
+    menuOpened &&
+    !e.composedPath().includes(headerButton) &&
+    !e.composedPath().includes(headerMenu)
+  )
+    menuToggle();
+};
